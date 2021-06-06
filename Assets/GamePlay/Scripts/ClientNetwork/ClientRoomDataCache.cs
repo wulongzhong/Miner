269161221b9b32f4ffa7c61ace8dc3602a4ca247
longzhongwu <1611136987@qq.com>
2021-06-06 09:40:06 +0800

    network

---
+++ Assets/GamePlay/Scripts/ClientNetwork/ClientRoomDataCache.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientRoomDataCache : MonoBehaviour {
+    public static ClientRoomDataCache Instance;
+
+    public void saveCache() {
+
+    }
+    public void initCache() {
+
+    }
+}
