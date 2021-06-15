de172c0e2a588b9af9f3e4ae8f2ba32bace810dc
longzhongwu <1611136987@qq.com>
2021-06-15 12:19:55 +0800

    server

--- GameUserServer/MsgType.cs
+++ GameUserServer/MsgType.cs
@@ -4,19 +4,7 @@ using System.Collections.Generic;
 
 public static class MsgType {
     private static List<Type> m_lstMsgType = new List<Type>() {
-        typeof(MsgPB.GameRoomPlayerLoginC2S),
-        typeof(MsgPB.GameRoomPlayerLoginS2C),
-        
-        typeof(MsgPB.GameRoomHeartBeatC2S),
-        typeof(MsgPB.GameRoomHeartBeatS2C),
 
-        typeof(MsgPB.GameRoomCache),
-
-        typeof(MsgPB.GameCommandInfo),
-        typeof(MsgPB.GameCommandRetrieveC2S),
-        typeof(MsgPB.GameCommandRetrieveErrorS2C),
-        typeof(MsgPB.GameCommandS2C),
-        typeof(MsgPB.GameFrameAllCommandInfo),
     };
 
     public static ushort getTypeId(Type type) {
